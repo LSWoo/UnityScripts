b9d1696a7bfad9c6501cf0d8a21294c0732a53c1
LSWoo <84316738+LSWoo@users.noreply.github.com>
2022-03-14 18:51:22 +0900

    Update DataManager.cs

--- Manager/DataManager.cs
+++ Manager/DataManager.cs
@@ -8,6 +8,7 @@ public class DataManager
 
     public void SetWeapon()
     {
+// 구글 쓰레드시트를 이용해 구현
         WeaponList.Add(new Weapon("Axe", 100001, 100f, 15f, 0, 0, 0));
         WeaponList.Add(new Weapon("Knife", 100002, 100f, 10f, 0, 0, 0));
         WeaponList.Add(new Weapon("Shovel", 100003, 100f, 10f, 0, 0, 0));
