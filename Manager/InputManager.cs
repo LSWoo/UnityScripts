868bd53813a1e20972440b472436c6db7bfba48a
LSWoo <84316738+LSWoo@users.noreply.github.com>
2022-03-15 05:07:11 +0900

    Update InputManager.cs

--- Manager/InputManager.cs
+++ Manager/InputManager.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class InputManager
 {
     #region KeySetting
-    public KeyCode InventoryKey = KeyCode.Tab;
+    public KeyCode InventoryKey = KeyCode.Tab; // 인벤토리 열기
+    public KeyCode GetItemKey = KeyCode.E; // 아이템 줍기
     #endregion
 
     public Action KeyAction = null;
