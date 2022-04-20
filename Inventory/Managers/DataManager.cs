ddbe48c6fe95a7abdc734fdc30029ae88cae8740
LSWoo <84316738+LSWoo@users.noreply.github.com>
2022-04-20 22:19:02 +0900

    Add files via upload

---
+++ Inventory/Managers/DataManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DataManager
+{
+    public List<ItemInfo> ItemInvenList = new List<ItemInfo>();
+
+    public List<ItemInfo> ItemDataList = new List<ItemInfo>();
+}
